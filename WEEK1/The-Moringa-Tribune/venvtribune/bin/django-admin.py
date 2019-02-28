95f5cf8dce85eac989f3bbcf5e9ea3ea1c575984
jinka <voiphandy@gmail.com>
2019-02-28 10:32:36 +0300

    Push to Github

---
+++ WEEK1/The-Moringa-Tribune/venvtribune/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/daud/Desktop/moringa-school/Core/Python/Django/The-Moringa-Tribune/WEEK1/The-Moringa-Tribune/venvtribune/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
